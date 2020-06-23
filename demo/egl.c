115580da5f26b801ce36ff715bda4f53fc61060c
Jonas Ådahl <jadahl@gmail.com>
2020-06-23 10:30:25 +0200

    demo/egl: Copy triangle code from weston's simple-egl
    Some minor changes:
    
     * Simplify swap buffer (don't use swap region)
     * Draw opaque when maximized or fullscreen
    
    This changes the EGL example to animate, as well as changes from OpenGL
    to OpenGL ES 2.

--- demo/egl.c
+++ demo/egl.c
@@ -1,15 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
 #include <EGL/egl.h>
 #include <wayland-client.h>
 #include <wayland-egl.h>
-#include <GL/gl.h>
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
 
 #include "libdecoration.h"
 #include "utils.h"
 
 static const size_t default_size = 200;
+
+static const char *vert_shader_text =
+	"uniform mat4 rotation;\n"
+	"attribute vec4 pos;\n"
+	"attribute vec4 color;\n"
+	"varying vec4 v_color;\n"
+	"void main() {\n"
+	"  gl_Position = rotation * pos;\n"
+	"  v_color = color;\n"
+	"}\n";
+
+static const char *frag_shader_text =
+	"precision mediump float;\n"
+	"varying vec4 v_color;\n"
+	"void main() {\n"
+	"  gl_FragColor = v_color;\n"
+	"}\n";
 
 struct client {
 	struct wl_display *display;
@@ -26,8 +46,16 @@ struct window {
 	EGLSurface egl_surface;
 	int content_width;
 	int content_height;
+	bool draw_opaque;
 	bool wait_for_configure;
 	bool open;
+
+	struct {
+		GLuint rotation_uniform;
+		GLuint pos;
+		GLuint col;
+	} gl;
+	uint32_t benchmark_time, frames;
 };
 
 static void
@@ -38,6 +66,7 @@ frame_configure(struct libdecor_frame *frame,
 	struct window *window = user_data;
 	struct libdecor_state *state;
 	int width, height;
+	enum libdecor_window_state window_state = LIBDECOR_WINDOW_STATE_NONE;
 
 	if (!libdecor_configuration_get_content_size(configuration, frame,
 						     &width, &height)) {
@@ -51,6 +80,23 @@ frame_configure(struct libdecor_frame *frame,
 	wl_egl_window_resize(window->egl_window,
 			     window->content_width, window->content_height,
 			     0, 0);
+
+	libdecor_configuration_get_window_state(configuration, &window_state);
+	if (window_state &  (LIBDECOR_WINDOW_STATE_MAXIMIZED |
+			     LIBDECOR_WINDOW_STATE_FULLSCREEN)) {
+		struct wl_region *region;
+
+		window->draw_opaque = true;
+		region = wl_compositor_create_region(window->client->compositor);
+		wl_region_add(region, 0, 0,
+			      window->content_width,
+			      window->content_height);
+		wl_surface_set_opaque_region(window->surface, region);
+		wl_region_destroy(region);
+	} else {
+		window->draw_opaque = false;
+		wl_surface_set_opaque_region(window->surface, NULL);
+	}
 
 	state = libdecor_state_new(width, height);
 	libdecor_frame_commit(frame, state, configuration);
@@ -120,16 +166,82 @@ static const struct wl_registry_listener registry_listener = {
 	registry_global_remove
 };
 
+static GLuint
+create_shader(struct window *window, const char *source, GLenum shader_type)
+{
+	GLuint shader;
+	GLint status;
+
+	shader = glCreateShader(shader_type);
+
+	glShaderSource(shader, 1, (const char **) &source, NULL);
+	glCompileShader(shader);
+
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+	if (!status) {
+		char log[1000];
+		GLsizei len;
+		glGetShaderInfoLog(shader, 1000, &len, log);
+		fprintf(stderr, "Error: compiling %s: %.*s\n",
+			shader_type == GL_VERTEX_SHADER ? "vertex" : "fragment",
+			len, log);
+		exit(1);
+	}
+
+	return shader;
+}
+
+static void
+init_gl(struct window *window)
+{
+	GLuint frag, vert;
+	GLuint program;
+	GLint status;
+
+	frag = create_shader(window, frag_shader_text, GL_FRAGMENT_SHADER);
+	vert = create_shader(window, vert_shader_text, GL_VERTEX_SHADER);
+
+	program = glCreateProgram();
+	glAttachShader(program, frag);
+	glAttachShader(program, vert);
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &status);
+	if (!status) {
+		char log[1000];
+		GLsizei len;
+		glGetProgramInfoLog(program, 1000, &len, log);
+		fprintf(stderr, "Error: linking:\n%.*s\n", len, log);
+		exit(1);
+	}
+
+	glUseProgram(program);
+
+	window->gl.pos = 0;
+	window->gl.col = 1;
+
+	glBindAttribLocation(program, window->gl.pos, "pos");
+	glBindAttribLocation(program, window->gl.col, "color");
+	glLinkProgram(program);
+
+	window->gl.rotation_uniform =
+		glGetUniformLocation(program, "rotation");
+}
+
 static bool
 setup(struct window *window)
 {
 	static const EGLint config_attribs[] = {
 		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-		EGL_RED_SIZE, 8,
-		EGL_GREEN_SIZE, 8,
-		EGL_BLUE_SIZE, 8,
-		EGL_ALPHA_SIZE, 8,
-		EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+		EGL_RED_SIZE, 1,
+		EGL_GREEN_SIZE, 1,
+		EGL_BLUE_SIZE, 1,
+		EGL_ALPHA_SIZE, 1,
+		EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+		EGL_NONE
+	};
+	static const EGLint context_attribs[] = {
+		EGL_CONTEXT_CLIENT_VERSION, 2,
 		EGL_NONE
 	};
 
@@ -137,32 +249,38 @@ setup(struct window *window)
 	EGLint n;
 	EGLConfig config;
 
-	window->client->egl_display = eglGetDisplay((EGLNativeDisplayType)window->client->display);
+	window->client->egl_display =
+		eglGetDisplay((EGLNativeDisplayType) window->client->display);
 
-	if (eglInitialize(window->client->egl_display, &major, &minor) == EGL_FALSE) {
+	if (eglInitialize(window->client->egl_display,
+			  &major, &minor) == EGL_FALSE) {
 		fprintf(stderr, "Cannot initialise EGL!\n");
 		return false;
 	}
 
-	if (eglBindAPI(EGL_OPENGL_API) == EGL_FALSE) {
+	if (eglBindAPI(EGL_OPENGL_ES_API) == EGL_FALSE) {
 		fprintf(stderr, "Cannot bind EGL API!\n");
 		return false;
 	}
 
-	if (eglChooseConfig(window->client->egl_display, config_attribs, &config, 1, &n) == EGL_FALSE) {
+	if (eglChooseConfig(window->client->egl_display,
+			    config_attribs, &config, 1, &n) == EGL_FALSE) {
 		fprintf(stderr, "No matching EGL configurations!\n");
 		return false;
 	}
 
-	window->client->egl_context = eglCreateContext(window->client->egl_display,
-						       config, EGL_NO_CONTEXT, NULL);
+	window->client->egl_context =
+		eglCreateContext(window->client->egl_display,
+				 config, EGL_NO_CONTEXT,
+				 context_attribs);
 
 	if (window->client->egl_context == EGL_NO_CONTEXT) {
 		fprintf(stderr, "No EGL context!\n");
 		return false;
 	}
 
-	window->surface = wl_compositor_create_surface(window->client->compositor);
+	window->surface =
+		wl_compositor_create_surface(window->client->compositor);
 
 	window->egl_window = wl_egl_window_create(window->surface,
 						  default_size, default_size);
@@ -172,8 +290,12 @@ setup(struct window *window)
 				      (EGLNativeWindowType)window->egl_window,
 				      NULL);
 
-	eglMakeCurrent(window->client->egl_display, window->egl_surface,
-		       window->egl_surface, window->client->egl_context);
+	eglMakeCurrent(window->client->egl_display,
+		       window->egl_surface,
+		       window->egl_surface,
+		       window->client->egl_context);
+
+	init_gl(window);
 
 	return true;
 }
@@ -181,10 +303,69 @@ setup(struct window *window)
 static void
 draw(struct window *window)
 {
-	glClearColor(1,0,0,1);
+	static const GLfloat verts[3][2] = {
+		{ -0.5, -0.5 },
+		{  0.5, -0.5 },
+		{  0,    0.5 }
+	};
+	static const GLfloat colors[3][3] = {
+		{ 1, 0, 0 },
+		{ 0, 1, 0 },
+		{ 0, 0, 1 }
+	};
+	GLfloat angle;
+	GLfloat rotation[4][4] = {
+		{ 1, 0, 0, 0 },
+		{ 0, 1, 0, 0 },
+		{ 0, 0, 1, 0 },
+		{ 0, 0, 0, 1 }
+	};
+	static const uint32_t speed_div = 5, benchmark_interval = 5;
+	struct timeval tv;
+	uint32_t time;
+
+	gettimeofday(&tv, NULL);
+	time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	if (window->frames == 0)
+		window->benchmark_time = time;
+	if (time - window->benchmark_time > (benchmark_interval * 1000)) {
+		printf("%d frames in %d seconds: %f fps\n",
+		       window->frames,
+		       benchmark_interval,
+		       (float) window->frames / benchmark_interval);
+		window->benchmark_time = time;
+		window->frames = 0;
+	}
+
+	angle = (time / speed_div) % 360 * M_PI / 180.0;
+	rotation[0][0] =  cos(angle);
+	rotation[0][2] =  sin(angle);
+	rotation[2][0] = -sin(angle);
+	rotation[2][2] =  cos(angle);
+
+	glViewport(0, 0, window->content_width, window->content_height);
+
+	glUniformMatrix4fv(window->gl.rotation_uniform, 1, GL_FALSE,
+			   (GLfloat *) rotation);
+
+	if (window->draw_opaque)
+		glClearColor(0.0, 0.0, 0.0, 1.0);
+	else
+		glClearColor(0.0, 0.0, 0.0, 0.5);
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	glVertexAttribPointer(window->gl.pos, 2, GL_FLOAT, GL_FALSE, 0, verts);
+	glVertexAttribPointer(window->gl.col, 3, GL_FLOAT, GL_FALSE, 0, colors);
+	glEnableVertexAttribArray(window->gl.pos);
+	glEnableVertexAttribArray(window->gl.col);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glDisableVertexAttribArray(window->gl.pos);
+	glDisableVertexAttribArray(window->gl.col);
+
 	eglSwapBuffers(window->client->egl_display, window->egl_surface);
+	window->frames++;
 }
 
 int
@@ -224,9 +405,6 @@ main(int argc, char *argv[])
 
 	wl_display_roundtrip(client->display);
 	wl_display_roundtrip(client->display);
-
-	// wait for the first configure event
-	//ret = wl_display_dispatch(client->display);
 
 	while (window->open && ret != -1) {
 		if (window->wait_for_configure) {
