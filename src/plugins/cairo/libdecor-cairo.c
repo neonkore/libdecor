2b15f44ce52affde0ed4a078606bdf0f7fe5e554
Christian Rauch <Rauch.Christian@gmx.de>
2021-03-02 00:45:32 +0000

    add myself to copyright header

--- src/plugins/cairo/libdecor-cairo.c
+++ src/plugins/cairo/libdecor-cairo.c
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2018 Jonas Ådahl
+ * Copyright © 2019 Christian Rauch
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
